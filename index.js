ff6ce6e9f300132b895a138c189f372ceeb56f6e
Zuzana Borutova <z.kasikova@gmail.com>
2021-06-04 20:39:37 +0200

    JS inspirace- vztahy

--- index.js
+++ index.js
@@ -1,3 +1,107 @@
-'use strict';
+('use strict');
 
 console.log('%c JavaScript funguje!', 'background-color: #000; color: #ff0');
+
+const vztahy = [
+  {
+    nazev: 'Sebeláska',
+    klicovaSlova: 'Sebeláska',
+    dozviteSe:
+      'Nejeduležitější vztah, je vztah k sobě samému. Zde se dozvíte, jak sebeláska ovlivňuje životní spokojenost a celkově kvalitu života.',
+    clanek:
+      'CO JE TO SEBELÁSKA? Sebeláska je o přijetí sebe samého jako jednoho velkého celku. Bezpodmínečná láska k sobě samému znamená přijetí své tělesné, mentální i duševní podstaty bytí a to jak svou světlou, tak i temnou stránku sebe sama.Sebeláska je o tom, jaký mátevztah k sobě. Jde o to, jak dokážete přijmout a milovat sám sebe a všechny části svého bytí. To znamená přijmout a milovat jak svou temnou stránku, tak i tu světlou. Znamená to mít velký respekt sám k sobě, ke svému tělu, k vlastní mysli, ke svému štěstí i své cestě. Jde o to starat se o své potřeby, namísto jejich obětování na úkor ostatních, aby jste se jim zavděčili. Jde o to znát svou hodnotu, mít správně nastavené hranice a umět říct ne.     Podstata sebelásky znamená žít v respektu, míru, pokoře, soucitu a lásce se všemi dalšími bytostmi, přírodou, zvířaty i matce zemi a hlavně sám k sobě. Jestliže bezpodmínečně milujete sebe samého, přijímáte své slabiny i nedostatky jako součást své podstaty a svého bytí.    PŘEVLEČENÁ SEBELÁSKA     Mnoho lidí si myslí, že se milují, ale to co skutečně cítí je spíše taková ta převlečená sebeláska. Je to spíš iluze, protože se milují jen za určitých podmínek. Například, když se Vám podaří zhubnout, když si uděláte vlasy a namalujete obličej, tak vypadáte skvěle a máte rádi sami sebe. Když dosáhnete něčeho velkého máte rádi sami sebe. Když pomůžete jiné osobě máte rádi sami sebe.    Ale co když se stane opak?    Co když přiberete pár kilo, stále byste se dívali do zrcadla na sebe s láskou a vděčností? Máte rádi sama sebe i když jsou vaše vlasy špinavé a mastné?    To, co často bereme jako sebelásku je často jen iluze, protože milujeme jen svou světlou stránku. Někdy to zajde tak daleko, že milujeme svou masku a ne kým skutečně jsme. Nemůžeme oddělovat svou temnou stránku a milovat a přijímat jen světlou stránku sebe samého. Sebeláska znamená, že se přijímám takový, jaký jsem i se všemi svými chybami i svou stinnou stránkou.    Místo čisté a bezpodmínečné lásky soudíme sami sebe. Pokud uděláte něco správného, ​​tak se odměníte láskou. Ale když uděláte něco špatného ​​potrestáte se tím, že si lásku vezmete. Stali jsme se sami pro sebe nástrojem na trestání a trestáme sami sebe. To není skutečná bezpodmínečná sebeláska, ale trýznění sebe samého.   JAK SE PROJEVUJE SEBELÁSKA V ŽIVOTĚ    Sebeláska má mnoho forem, jak se projevit, ale můžeme je shrnout do pětí aspektů, které nám ukazují jak jsme na tom s mírou sebelásky v životě. Tyto aspekty pokrývají různé úrovně nás samotných a našeho vztahu nejen k sobě či k okolnímu světu.    Láska a přijetí vlastního těla takové jaké je. Jde o lásku ke svému tělu a všem jeho součástem.     Přijetí své skutečné podstaty, láska ke své duši a vyššímu principu. To znamená, že nasloucháme svému srdci, protože skrze srdce k nám promlouvá naše duše. Jde o umění žít v souladu se svým srdcem. V praxi to znamená být sám sebou, znát skutečné vnitřní hodnoty, nechat skrze sebe projevit tvořivost a kreativní energie, dělat to co mě baví a naplňuje.    Láska ke své mysli. Jde o zralý způsob myšlení a přístup nejen k sobě, ale i ostatním lidem a celému světu. Uvědomění toku energie a sílu myšlenek, kterou používám pro tvoření, spolupráci namísto ničení a ubližování a je jedno jestli ve vztahu k sobě, nebo ostatním.    Pochopení bytí a láska k němu. Jde o bezpodmínečnou lásku vůči sobě a celé své duchovní podstatě. Jde o lásku k celé své bytosti na všech úrovních a přijetí svého principu. Přijetí sebe i se všemi nedokonalostmi a temnými stránkami.    Láska a soucit k okolnímu světu. To znamená milovat, přijímat a také respektovat sebe, svůj život i celý svět včetně přírody, zvířat, druhých lidi i matky země na které žijeme. Jde o pochopení propojenosti a jednoty, protože jsme všichni propojení.    VLIV SEBELÁSKY NA TVŮJ ŽIVOT   Sebeláska je něco, co se nás dotýká ve všech oblastech života a na všech úrovních. Pojďme se podívat na ty nejdůležitější výhody sebelásky na náš život:    Lepší kvalita tvých vztahů    Zdravý a silný vztah k sobě    Šťastnější a spokojenější život    Sebevědomí    Autentičnost (umění být sám sebou)    Umět říct ne    Zdravé vnitřní hranice    Mít sám sebe na prvním místě    Znáte své silné stránky    Víte kdo jste a kým chcete být    Přijímáte sami sebe takové jací jste i se svými chybami a nedostatky    Umění říct svůj vlastní názor nahlas a stát si za ním    Soucit vůči sobě, ale i ostatním(lidi, zvířata, příroda)    Lepší fyzické i duševní zdraví    Naplnění vlastních potřeb    Budete osvobozeni od potřeby hledat lásku a vnitřního naplnění z vnějšího světa od ostatních lidí    Budete znát svou hodnotu    Sebeláska má velké spektrum a velký vliv na náš život. Ovlivňuje naše vztahy, to, jaké máme sebevědomí a jestli známe své silné stránky a svůj potenciál, což ovlivňuje náš pracovní i finanční život. Ale také velmi ovlivňuje naše zdraví.     Štěstí, to jak jsme v životě šťastní se odvíjí od kvality naších vztahů. Znamená to, že jsme zase u sebelásky. Za nejčastější příčinou nemocí a zdravotních problémů stojí právě naše vztahy. I samotná psychosomatika nám to ukazuje jak velmi důležité jsou naše vztahy a jejich kvalita pro naše zdraví.    Zdroj: https://cestazasnem.com/sebelaska/ ',
+    id: 1,
+  },
+  {
+    nazev: 'Matka x Dcera',
+    klicovaSlova: 'Rodiče, Rodina, Matka',
+    dozviteSe:
+      'Vztah matky s dcerou může mít mnoho podob. Může být něžný, laskavý, kamarádský, spiklenecký, partnerský, ale také rozhádaný, nepochopený, žárlivý či bez vzájemného respektu. Obě, jak matka, tak i dcera, jsou obecně jako ženy emočně založené. Do vztahu se promítají emoce jak matky, tak i dcery a vše se velmi rádo a často střídá. Dcery chtějí patřit do ženského světa a mámy by je v něj měly citlivě uvést.',
+    clanek:
+      'Matka je pro dceru vzorem. Matka je pro dceru vzorem i inspirací. Podle matčina příkladu si dcera vytváří představu o tom, jak bude v budoucnu chtít vést svůj vlastní život. Toto se děje přijetím, ale i odmítnutím matčiných stereotypů a vzorců. Velmi pravděpodobně se dcera později také stane matkou a vědomě či nevědomě bude předávat výchovné vzorce od své matky vlastním dětem. Nebo se naopak proti výchově ze strany vlastních rodičů zcela vyhraní a cíleně bude vychovávat vlastní děti naprosto odlišně. Nebo něco mezi tím. Proto je důležité, aby matka měla jasnou představu o tom, jaký vztah chce se svou dcerou mít. Zda se chce k dceři v dospělosti stavět, jako k rovnocenné partnerce nebo bude požadovat autoritu matky.  Cesta k partnerství Cesta k vzájemnému partnerství mezi matkou a dcerou je ale dlouhá. Nejprve vnímá matka svou dceru jako dítě. Samozřejmě. Matka je ta, která svět zná a vysvětluje dceři, jak to chodí. Ale z človíčka, který je zcela podřízen, roste samostatný a nezávislý jedinec a autoritu střídá partnerství. Potřeba rovnocenného vztahu dcery s matkou roste s věkem. Na této cestě k partnerství je několik zásadních mezníků, které vztah mimořádně posouvají. Puberta Jedním z nich je počátek puberty. Doba bouřlivá, ve které se obě, matka i dcera, vzájemně vymezují. Dceřina vynořující se osobnost o sobě dává hlasitě vědět a rychlé hádky kvůli hloupostem jsou na denním pořádku. Tady je důležité pro mámu uvidět rozdíl mezi úkolováním dcery a partnerskou spoluprací. Dobrým výsledkem je, když máma o své patnáctileté dceři přemýšlí, že se zvládne o sebe postarat, i když finančně je stále na rodičích závislá. Zvládá běžnou starost o domácnost, vyžehlí si, vypere, uvaří, zařídí si běžné věci, postará se o školu.  Sobě i okolí takto dává najevo svou samostatnost a posiluje to její sebedůvěru. Pusťte dceru do svého království Pro dceru není úplně snadné najít si v domácnosti a rodině vlastní místo, prostor, sféru působení. Ženské funkce zde zastává matka a ona jako rozkvétající žena začíná mít touhu vlastního uplatnění. Je velmi důležité, aby matka své dceři dokázala nějaký prostor uvolnit. Dospělá dcera Ve vzájemném vztahu přijde doba, kdy by se dcera měla začít přirozeně odpojovat. Měla by se začít starat o sebe bez Vašeho vlivu. Být zodpovědná za sebe a za své důsledky chování. Toto je asi nejtěžší úkol pro každého rodiče. Pustit dítě ze svého vlivu a kontroly. Věřit mu, že to nějak zvládne, i když žije život jiný, než jste si představovali Vy. Umožnit mu dospět. V období puberty a dospívání dcera potřebuje mít možnost dovolit si klasické pubertální projevy, které k tomuto období patří. Vymezovat se vůči rodiči, zkoušet hranice, sem tam si vymýšlet a překrucovat své pravdy, nedodržet domluvená pravidla, koupit si něco, co se Vám vůbec nelíbí. Toto jsou všechno kroky, které vedou k osamostatnění. Skrze tyto rebelie poznává samu sebe. Ideálním cílem je, aby vystoupila z Vašeho područí a stala se svébytnou, zodpovědnou a sebevědomou ženou. Dcera si potřebuje uvědomit, že se o sebe umí postarat a že není na Vás závislá. Potřebuje vědět, jak chce žít, čeho chce dosáhnout a z čeho bude platit životní náklady. Ale také potřebuje mít důvěru v to, že ji dokážete podpořit. Slovem nebo činem. Vztah mezi matkou a dcerou je jedinečný a silný Vztah mezi matkou a dcerou je jedinečný a silný, je základem pro celý další život. Obě jste ženy, a ty dokážou udělat svět krásnější a bohatší. Tak si to užívejte! Povídejte si o ženství, o tom, co to je, být ženou, běžte spolu nakupovat, kujte pikle na tatínky a nežárlete, když jde s problémem za ním. Jsou věci, které se potřebuje naučit od táty a pak ty všechny ostatní, které jí dáte Vy. Zdroj: https://www.akademierodicovstvi.cz/blog/cesta-k-vzajemnemu-partnerstvi-mezi-matkou-a-dcerou ',
+    id: 2,
+  },
+  {
+    nazev: 'Přátelství',
+    klicovaSlova: 'Přátelství',
+    dozviteSe:
+      'Možná i vy znáte lidi, kteří si vystačí sami nebo se spokojí jen s nejbližšími příbuznými. Přátele moc nepotřebují, nevyhledávají je, říkají, že pro ně nehrají příliš velkou roli.  A pak jsou lidé, kteří jsou důsledkem své povahy a hlavně životních zkušeností přátelštější, blízkým vztahům více důvěřují a přinášejí jim radost, klid a bezpečí. Pro ně je přátelství jednou z nejdůležitějších hodnot v životě. Ať už patříte do té či oné skupiny, jednoznačně platí, že přátelské vazby jsou důležité pro většinu z nás.    ',
+    clanek: '',
+    id: 3,
+  },
+  {
+    nazev: 'Spokojený partnerský vztah',
+    klicovaSlova: 'Partnerství, spokojený vztah, láska',
+    dozviteSe:
+      'Při hledání partnera časem zjistíme, že některé vlastnosti mají na vzájemné soužití jen malý vliv, zatímco jiné ovlivňují vztah zásadně. Jakou vlastnost je tedy důležité hledat? Kdybyste měli vypíchnout pouze jednu charakteristiku, která partnerovi nesmí chybět, co by to bylo? Odpovědi se mohou různit, jako jsme různí my. Tak která vlastnost je ta nejdůležitější pro mě?',
+    clanek: '',
+    id: 4,
+  },
+  {
+    nazev: 'Člověk a zvíře',
+    klicovaSlova: 'Zvire',
+    dozviteSe:
+      'Se vztahem člověk – zvíře se setkáváme na nejrůznějších úrovních už od pravěku. Zpočátku člověk zvířata lovil, později je – zejména některé druhy savců a ptáků – domestikoval. I dnes mají zvířata důležitou úlohu v různých odvětvích lidské činnosti.',
+    clanek: '',
+    id: 5,
+  },
+  {
+    nazev: 'Vztah s rodiči',
+    klicovaSlova: 'Rodiče, rodinné vztahy',
+    dozviteSe:
+      'Naprostá většina vztahových problémů pramení z našich primárních vztahů, tj. z těch, které máme se svými rodiči. Odvozujeme z nich naše očekávání vůči druhým lidem i ta, které máme sami vůči sobě. A to, co si s rodiči nevyřešíme, si v sobě nosíme všude s sebou a v takové podobě, v jaké jsme ustrnuli.',
+    clanek: '',
+    id: 6,
+  },
+  {
+    nazev: 'Naplněný vztah',
+    klicovaSlova: 'Láska',
+    dozviteSe:
+      'Vztahy nás obklopují. Nelze nemít k někomu nebo něčemu vztah. Vždy nějaký existuje, ať už je dobrý nebo špatný nebo všelijaký. Partnerské vztahy jsou pro nás většinou ty nejdůležitější, a proto je také potřeba, aby i pro náš pohodový život dobře fungovaly.Přečtěte si tipy jak prožívat naplněný vztah.',
+    clanek: '',
+    id: 7,
+  },
+  {
+    nazev: 'Důvěra',
+    klicovaSlova: 'Důvěra',
+    dozviteSe:
+      'Důvěra – těžko se získává, lehce se dá ztratit. Pokud ji ztratíte, trvá velmi dlouho než vám například blízký člověk začne znovu věřit.',
+    článek: '',
+    id: 8,
+  },
+  {
+    nazev: 'Potřeba uznání',
+    klicovaSlova: 'Uznání',
+    dozviteSe:
+      'Lásku potřebujete nejvíce tehdy, když máte pocit, že si ji nejméně zasloužíte.Všichni si přejeme lásku a uznání a občas jsme pro to ochotni udělat téměř cokoli. Občas se setkávám s tím, že nedostatek těchto citů způsobuje silnou potřebu dokázat, že za něco stojíme. V tomto případě ale vycházíme ze strachu a touze po potvrzení, nikoli z vlastního sebevědomí.',
+    clanek: '',
+    id: 9,
+  },
+  {
+    nazev: 'Sebeláska ve vztahu',
+    klicovaSlova: 'Sebeláska, láska, vztah ',
+    dozviteSe:
+      'Ve vztahu je sebeláska tím absolutně nejdůležitějším. Všechny naše vztahy a jejich kvalita je závislá a přímo úměrná na tom, jaký máme vztah sami k sobě. Pokud sami sebe nepřijímáme a nemilujeme, tak náš žádný partner ani nikdo jiný nemůže přijímat a milovat. ',
+    článek: '',
+    id: 10,
+  },
+];
+
+const Clanek = (props) => {
+  return `
+  <h1 class="nazev__clanku">${props.nazev}</h1>
+  <p class="dozvite__se">${props.dozviteSe}</p>
+  <div class=clanek__vztahy"${props.clanek}</div>
+  `;
+};
+
+const Clanky = (props) => {
+  let result = '';
+  for (let i = 0; i < props.list.length; i++) {
+    result += Clanek(props.list[i]);
+  }
+  return result;
+};
+
+const clanekElm = document.querySelector('#clanek');
+clanekElm.innerHTML = Clanky({ list: vztahy });
